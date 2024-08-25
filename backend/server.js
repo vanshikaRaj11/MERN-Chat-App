025964b7d762d1d179b0475c457f7dd27d4d9b71
Vanshika Rajput <vanshikavce19@gmail.com>
2024-08-25 12:24:35 +0530

    First API endpoints created

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const dotenv = require('dotenv');
 const { chats } = require("./data/data");
+
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("API is running");
@@ -10,7 +13,11 @@ app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
 
-// app.get("/api/chat/:id", (req, res) => {
-//   console.log(req);
-// });
-app.listen(8000, console.log("Server Started on PORT 8000"));
+app.get("/api/chat/:id", (req, res) => {
+  // console.log(req.params.id);
+  const singeChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singeChat);
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
