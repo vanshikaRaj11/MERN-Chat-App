6ef3df800be20a8f9df7ffbecc2cba3662956767
Vanshika Rajput <vanshikavce19@gmail.com>
2024-08-26 17:49:45 +0530

    Database setup done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require('dotenv');
 const { chats } = require("./data/data");
+const connectDB = require("./config/database");
+const colors = require('colors')
 
-const app = express();
 dotenv.config();
+connectDB()
+const app = express();
+
 
 app.get("/", (req, res) => {
   res.send("API is running");
@@ -20,4 +24,4 @@ app.get("/api/chat/:id", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
+app.listen(PORT, console.log(`Server Started on PORT ${PORT}`.yellow.bold));
