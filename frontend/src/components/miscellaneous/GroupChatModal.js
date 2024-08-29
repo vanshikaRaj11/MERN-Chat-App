a429c1fdf9b368dff631efb6540f56d3828eb9c2
Vanshika Rajput <vanshikavce19@gmail.com>
2024-08-29 21:46:07 +0530

    Issue fixed

---
+++ frontend/src/components/miscellaneous/GroupChatModal.js
@@ -0,0 +1,119 @@
+import {
+  Button,
+  FormControl,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import axios from "axios";
+import { ChatState } from "../../context/ChatProvider";
+import UserListItem from "../userAvatar/UserListItem";
+
+const GroupChatModal = ({ children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [groupChatName, setGroupChatName] = useState();
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState();
+  const [loading, setLoading] = useState();
+
+  const toast = useToast();
+
+  const { user, chats, setChats } = ChatState();
+  const handleSubmit = async () => {};
+  const handleSearch = async (query) => {
+    setSearch(query);
+    if (!query) {
+      return;
+    }
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      console.log("Data",data);
+      
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Search Results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+    };
+    
+    const handleGroup=() => {
+        
+    }
+
+  return (
+    <>
+      <span onClick={onOpen}>{children}</span>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader
+            fontSize="35px"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent="center"
+          >
+            Create Group Chat
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody display="flex" flexDir="column" alignItems="center">
+            <FormControl>
+              <Input
+                placeholder="Chat Name"
+                mb={3}
+                onChange={(e) => setGroupChatName(e.target.value)}
+              />
+            </FormControl>
+
+            <FormControl>
+              <Input
+                placeholder="Add Users"
+                mb={1}
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </FormControl>
+            {loading ? (
+              <div>loading...</div>
+            ) : (
+              searchResult
+                ?.slice(0, 4)
+                  .map((user) => <UserListItem key={user._id} user={user} handleFunction={() => handleGroup(user)}>
+                </UserListItem>)
+            )}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" onClick={handleSubmit}>
+              Create Chat
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default GroupChatModal;
