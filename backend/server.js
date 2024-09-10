a1de9a576a46782bce949a87abcc1500f49bb8d7
Vanshika Rajput <vanshikavce19@gmail.com>
2024-09-10 14:54:31 +0530

    Socket issue fixed

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "htps://localhost:3000",
+    origin: "http://localhost:3000",
   },
 });
 
